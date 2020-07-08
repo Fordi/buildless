5e99aa5ce857354055900926b9007a8941dea18c
Bryan Elliott <bryan_elliott@comcast.com>
2020-07-08 13:34:44 -0400

    Added htm-less `prod` export

---
+++ src/prod.js
@@ -0,0 +1,52 @@
+import {
+  render,
+  hydrate,
+  createElement,
+  h,
+  Fragment,
+  createRef,
+  Component,
+  cloneElement,
+  createContext,
+  toChildArray,
+  __u,
+  options
+} from 'preact';
+
+import {
+  useState,
+  useReducer,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useImperativeHandle,
+  useMemo,
+  useCallback,
+  useContext,
+  useDebugValue
+} from 'preact/hooks';
+
+export {
+  render,
+  hydrate,
+  createElement,
+  h,
+  Fragment,
+  createRef,
+  Component,
+  cloneElement,
+  createContext,
+  toChildArray,
+  __u,
+  options,
+  useState,
+  useReducer,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useImperativeHandle,
+  useMemo,
+  useCallback,
+  useContext,
+  useDebugValue
+};
