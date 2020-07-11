8200f3656f28d6e39e873ee677bc9caca46c42f2
Bryan Elliott <fordiman@gmail.com>
2020-07-11 14:57:44 -0400

    Skip wrapping div

--- docs/index.js
+++ docs/index.js
@@ -1,4 +1,4 @@
 import { render, html } from 'https://unpkg.com/@fordi-org/buildless';
 import App from './App.js';
 
-render(html`<${App}/>`, document.getElementById('app'));
+render(html`<${App}/>`, document.body);
